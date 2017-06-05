9042b1a2d62c76bb6383e852d269cf1c75fa9793
박다정 <daaa08@naver.com>
2017-06-05 16:04:01 +0900

    Googlemap 추가

--- app/src/main/java/com/example/da08/viewpager/FourFragment.java
+++ app/src/main/java/com/example/da08/viewpager/FourFragment.java
@@ -1,18 +1,36 @@
 package com.example.da08.viewpager;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FourFragment extends Fragment {
+public class FourFragment extends Fragment implements OnMapReadyCallback {
 
+
+
+    LocationManager manager = null;
+    GoogleMap map = null;
 
     public FourFragment() {
         // Required empty public constructor
@@ -23,7 +41,124 @@ public class FourFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_four, container, false);
+        View view = inflater.inflate(R.layout.fragment_four, container, false);
+        // 프래그먼트에서 맵 호출하기 ( 프래그먼트 안에서 다른 프래그먼트를 가져올때 )
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        // map 이 사용할 준비가되면 onMapReady메소드 호출
+        mapFragment.getMapAsync(this);
+
+        // 상위 액티비티의 자원을 사용하기위해서 액티비티를 가져옴
+        MainActivity activity = (MainActivity) getActivity();
+
+        manager = activity.getLocationManager();
+
+        return view;
     }
 
-}
+
+
+    /*
+    이 함수에서부터 map 을 그리기 시작
+     */
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        // 좌표만 생성
+        LatLng sinsa = new LatLng(37.5169583, 127.0208001);
+        // 좌표 적용
+        // 1 마커를 생성
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(sinsa);
+        marker.title("석봉토스트");
+        // 1.2 마커를 화면에 그린다
+        googleMap.addMarker(marker);
+
+        // 2 맵의 중심을 해당 좌표로 이동시킨다                         좌표 , 줌레벨
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sinsa, 12));
+
+    }
+
+        // 현재 프래그먼트가 러닝 직전
+        @Override
+        public void onResume() {
+            super.onResume();
+            // 마시멜로 이상 버전에서는 런타임권한 체크 여부를 확인해야함
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                // gps 사용을의한 권한 확득이 되어 있지 않으면 리스너 등록을 하지 않는다
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_CHECKIN_PROPERTIES)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+            }
+            // gps리스너 등록
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,// 위치 제공자
+                    3000, // 변경사항 체크 추기 밀리세컨드 단위
+                    1, // 변경사항 체크 거리  미터 단위
+                    locationListener
+            );
+        }
+
+        // 현재 프래그먼트가 정지
+
+        @Override
+        public void onPause() {
+            super.onPause();
+
+            // 마시멜로 이상 버전에서는 런타임권한 체크 여부를 확인해야함
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                // gps 사용을의한 권한 확득이 되어 있지 않으면 리스너를 해제하지 않는다
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_CHECKIN_PROPERTIES)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+            }
+            // 리스너 해제
+            manager.removeUpdates(locationListener);
+        }
+
+
+        // gps사용을 위해서 좌표 리스너를 생성해야 함
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                /*
+                 내 좌표에 변경사항이 있으면 좌표를 불러옴
+                 */
+                // 경도
+                double lng = location.getLongitude();
+                // 위도
+                double lat = location.getLatitude();
+                // 고도
+                double alt = location.getAltitude();
+                // 정확도
+                double acc = location.getAccuracy();
+                // 위치제공자
+                String provider = location.getProvider();
+
+                // 바뀐 현재 좌표
+                LatLng current = new LatLng(lat, lng);
+                // 현재 좌표로 카메라 이동
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 12));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extra) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+    }
+
+
--- app/src/main/java/com/example/da08/viewpager/MainActivity.java
+++ app/src/main/java/com/example/da08/viewpager/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.da08.viewpager;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +29,22 @@ public class MainActivity extends AppCompatActivity {
     Fragment three;
     Fragment four;
 
+    LocationManager manager;
+
+    public LocationManager getLocationManager(){
+        return manager;
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        // Context의 안에 있는 상수를 불러온 것
+
 
         // 1 뷰페이저 위젯 연결
         viewPager = (ViewPager) findViewById(R.id.viewPager);
@@ -58,6 +77,43 @@ public class MainActivity extends AppCompatActivity {
 
         // 7 탭이 변경되었을때 페이저를 변경해주는 리스너
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        }
+    }
+
+    // 위치 제공자 사용을위한 권한처리
+    private final int REQ_PERMISSION = 100;
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermission(){
+        //1 권한체크 - 특정권한이 있는지 시스템에 물어본다
+        if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+        }else{
+            // 2. 권한이 없으면 사용자에 권한을 달라고 요청
+            String permissions[] = { Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    , Manifest.permission.CAMERA };
+            requestPermissions(permissions ,REQ_PERMISSION); // -> 권한을 요구하는 팝업이 사용자 화면에 노출된다
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQ_PERMISSION){
+            // 3.1 사용자가 승인을 했음
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            }else{
+                cancel();
+            }
+        }
+    }
+
+    public void cancel(){
+        Toast.makeText(this, "권한요청을 승인하셔야 GPS를 사용할 수 있습니다.", Toast.LENGTH_SHORT).show();
+        finish();
     }
 
     class PagerAdapter extends FragmentPagerAdapter{
